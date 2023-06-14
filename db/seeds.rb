6f0ee8a11901120e33825566cf120e112bf72d3f
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-06-14 09:38:40 -0500

    seeded db and created simple views for index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Kitten.destroy_all
+
+name = [
+  'Saddam Hussein',
+  'Saddam Husseins best friend',
+  'Chairman Meow',
+  'Notorious C.A.T.',
+  'Mr. Bigglesworth',
+  'Catticus Finch',
+  'Pancake',
+  'Cleveland Cataliers',
+  'Isaac Meowton',
+  'Winston Furchill'
+]
+
+15.times do 
+  Kitten.create(
+    name: name.sample,
+    age: rand(1..15),
+    cuteness: rand(1..10),
+    softness: rand(1..10)
+  )
+end
