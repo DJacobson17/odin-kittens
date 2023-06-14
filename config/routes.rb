f9210bce4a3ec39a2130f2e646a53738feb5513d
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-06-14 10:20:22 -0500

    created form using simple_form and added more navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/new'
-  get 'kittens/create'
-  get 'kittens/show'
-  get 'kittens/edit'
-  get 'kittens/update'
-  get 'kittens/destroy'
+resources :kittens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
